d98a3548222d4d5db74b16e78cadd4a475ca06ba
n1colezhou <116459861+n1colezhou@users.noreply.github.com>
2022-10-23 06:55:55 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,87 @@
+import static com.teamdev.jxbrowser.engine.RenderingMode.*;
+
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.view.swing.BrowserView;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class GoogleMaps {
+
+    private static final int MIN_ZOOM = 0;
+    private static final int MAX_ZOOM = 21;
+
+    public static String setMarker(double x, double y) {
+        String setMarkerScript =
+            "var myLatlng = new google.maps.LatLng(" + x + "," + y + ");\n" +
+                    "var marker = new google.maps.Marker({\n" +
+                    "    position: myLatlng,\n" +
+                    "    map: map,\n" +
+                    "    title: 'Hello World!'\n" +
+                    "});";
+        return setMarkerScript;
+    }
+
+    /**
+     * In map.html file default zoom value is set to 4.
+     */
+    private static int zoomValue = 4;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(new File("coordinates.txt"));
+        String line = input.nextLine();
+        String lat = line.split(" ")[0];
+        String lon = line.split(" ")[1];
+        double x = Double.parseDouble(lat);
+        double y = Double.parseDouble(lon);
+
+        EngineOptions options =
+                EngineOptions.newBuilder(HARDWARE_ACCELERATED).build();
+        Engine engine = Engine.newInstance(options);
+        Browser browser = engine.newBrowser();
+
+        SwingUtilities.invokeLater(() -> {
+            BrowserView view = BrowserView.newInstance(browser);
+
+            JButton zoomInButton = new JButton("Zoom In");
+            zoomInButton.addActionListener(e -> {
+                if (zoomValue < MAX_ZOOM) {
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript("map.setZoom(" +
+                                    ++zoomValue + ")"));
+                }
+            });
+
+            JButton zoomOutButton = new JButton("Zoom Out");
+            zoomOutButton.addActionListener(e -> {
+                if (zoomValue > MIN_ZOOM) {
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript("map.setZoom(" +
+                                    --zoomValue + ")"));
+                }
+            });
+
+            JButton setMarkerButton = new JButton("Set Marker");
+            setMarkerButton.addActionListener(e ->
+                    browser.mainFrame().ifPresent(frame ->
+                            frame.executeJavaScript(setMarker(x, y))));
+
+            JPanel toolBar = new JPanel();
+            toolBar.add(zoomInButton);
+            toolBar.add(zoomOutButton);
+            toolBar.add(setMarkerButton);
+
+            JFrame frame = new JFrame("Google Maps");
+            frame.add(toolBar, BorderLayout.SOUTH);
+            frame.add(view, BorderLayout.CENTER);
+            frame.setSize(800, 500);
+            frame.setVisible(true);
+
+            browser.navigation().loadUrl("file:///Users/me/map.html");
+        });
+    }
+}
